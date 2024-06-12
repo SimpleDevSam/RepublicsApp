6149aaeef1c82c79c6541ea00dbb4d1da13135a4
Samuel Maia Salvador de Assis <samuelufop121035@gmail.com>
2024-06-12 06:36:47 -0300

    szz

--- DiscountContext.Tests/obj/Debug/net8.0/DiscountContext.Tests.AssemblyInfo.cs
+++ DiscountContext.Tests/obj/Debug/net8.0/DiscountContext.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscountContext.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546c0548c87f9be56f56841e44a2f1df139ed3b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscountContext.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscountContext.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
